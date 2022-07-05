4d61f91f0a61c9b52aa51d68e926c33a6d281054
wanbekgithub@gmail.com <wanbekgithub@gmail.com>
2022-07-05 20:00:47 +0100

    dd

--- src/scroll.js
+++ src/scroll.js
@@ -19,3 +19,27 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   setTimeout(fill, (truckLoopDur * 1e3) / 4);
 })
+
+
+console.log('Hello World!');
+
+
+
+
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  let percents = [0.25, 0.5, 0.75, 1];
+  step = 0;
+  truckLoopDur = 10;
+  fill = () => {
+    scrollRef.current.style.transform = "scaleY(" + percents[step] + ")";
+    ++step;
+    if (step < percents.length) {
+      setTimeout(fill, (truckLoopDur * 1e3) / 2);
+    }
+  }
+  setTimeout(fill, (truckLoopDur * 1e3) / 4);
+})
+
