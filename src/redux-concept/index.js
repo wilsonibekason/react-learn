46798cc54517a6840f4730484049c1951756d193
wilsonibekason <wanbekgithub@gmail.com>
2022-07-30 04:05:37 -0600

    git add redux-concept directory

---
+++ src/redux-concept/index.js
@@ -0,0 +1,69 @@
+//import { CREATE_POST } from "./actionTypes";
+const CREATE_POST = "CREATE_POST";
+const EDIT_POST = "EDIT_POST";
+const SET_FILTER = "SET_FILTER";
+
+const createPost = { type: CREATE_POST, user: "dan", text: "new post" };
+console.log(createPost);
+// ES2015 use of arrow function
+function producePost(user, text) {
+  return {
+    type: CREATE_POST,
+    user: user,
+    text: text,
+  };
+}
+
+console.log(producePost("dan", "compiled code"));
+
+const producePost1 = (user, text) => {
+  {
+    type: CREATE_POST, user, text;
+  }
+};
+
+// implementing Rducers for redux application
+const post = {
+  posts: [
+    { user: "wilson", category: "hello", text: "welcome to redux programming" },
+  ],
+  filter: "hello",
+};
+// postsreducer filterreducer and usersreduces
+const editPost = (id, text) => {
+  {
+    type: EDIT_POST, id, text;
+  }
+};
+
+// filterpost
+const setFilter = (filter) => {
+  {
+    type: SET_FILTER, filter;
+  }
+};
+
+// declaring the post reducers
+// user this  const {type, ...post} = action
+// const type = action.type const post = {user: action.user, category: action.category, text: action.text}
+const postReducer = (state = [], action) => {
+  switch (action?.type) {
+    case CREATE_POST: {
+      //const type  = action?.type
+      const { type, ...post } = action;
+      return [...state, post];
+      // use can also use the Array.prototype.concat()  which merges two arrays and returns a new one
+      // return state.concat([post])
+    }
+    // handling edit post
+    case EDIT_POST: {
+      // renaming propertiees and multiple desstructuring
+      // const { type: actionType, user = "wilson" } = action;
+      return state.map((oldpost, index) =>
+        action?.id === index ? { ...oldpost, ...newpost } : oldpost
+      );
+    }
+    case SET_FILTER: {
+    }
+  }
+};
---
+++ src/redux-concept/note.tst
@@ -0,0 +1,12 @@
+action creators are funtions thst return other actions
+
+
+N/B Try to structure your application  so that each feature has its own  substate of data to work on 
+
+when declaring a reducr firstly think ablout th function declaration
+reducer takes in the initial / default state which  could be an empty array obj the second paramter is the action
+
+// struture of reducers
+1 use of switc statements
+
+rest operator collects data while spread operator retrives or spreads that data
