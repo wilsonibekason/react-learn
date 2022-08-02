af99620581cc7762de067a25e0f38fe3685133cb
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-02 11:50:13 +0100

    complete initialise useReducer function

--- src/redux-concept/useReducer/reducer.jsx
+++ src/redux-concept/useReducer/reducer.jsx
@@ -4,12 +4,40 @@ const IncrementNumber = () => {
   const [num, setNum] = useState();
   const incrementNum = () => setNum((prevState) => prevState + 1);
   const decrementNum = () => setNum((prevState) => prevState - 1);
+  // declaring reducer actions
+  const ACTIONS = {
+    INCREMENT: "increment",
+    DECREMENT: "decrement",
+  };
+  const { INCREMENT, DECREMENT } = ACTIONS;
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case INCREMENT:
+        return { count: state.count + 1 };
+        break;
+      case DECREMENT:
+        return { count: state.count };
+    }
+  };
+  // TRYING OUT THE USErEDUUCER
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  // calling the function
+  dispatch({ type: DECREMENT });
+  const incre = () => {
+    dispatch({ type: INCREMENT });
+  };
+  const decre = () => {
+    dispatch({ type: DECREMENT });
+  };
   return (
     <>
       <div>
         <button onClick={() => incrementNum()}>Increment Number</button>
         <button onClick={() => decrementNum()}>Decrement Number</button>
+        <button onClick={incre}>Increment Number</button>
+        <button onClick={decre}>Decrement Number</button>
         <p> {num}</p>
+        <p> {state.count}</p>
       </div>
     </>
   );
