0891e7f465e8a87e182b9ea3b42fc838323cc9c1
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-11 04:44:18 +0100

    complete create

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 // it is recommmended to have a single store rather than multiple stores
-
+// slice is multiple reducer functios and action fpr a single feature in our app
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({ reducer: {} });
