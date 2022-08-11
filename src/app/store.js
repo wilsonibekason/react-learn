ed0df81e5a9c477fe18eb4c36b923a32ac5337e9
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-11 04:37:09 +0100

    install react-redux pkg

---
+++ src/app/store.js
@@ -0,0 +1,5 @@
+// it is recommmended to have a single store rather than multiple stores
+
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({ reducer: {} });
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PermissionConsumer from './PermissionProvider.jsx';
+import PermissionConsumer from "./PermissionProvider.jsx";
 import App from "./App";
-
+import { store } from "./app/store.js";
+import { Provider } from "react-redux";
 
 var mountNode = document.getElementById("app");
 ReactDOM.render(
   <PermissionConsumer>
-  <App name="Jane" />
-  </PermissionConsumer>
-  , mountNode);
+    <Provider store={store}>
+      <App name="Jane" />
+    </Provider>
+  </PermissionConsumer>,
+  mountNode
+);
