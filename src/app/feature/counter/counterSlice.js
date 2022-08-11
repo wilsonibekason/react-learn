aaad8821074e822b7f3225c45cf14da1034301e7
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-11 05:09:20 +0100

    complete simple counter app

--- src/app/feature/counter/counterSlice.js
+++ src/app/feature/counter/counterSlice.js
@@ -14,8 +14,16 @@ export const counterSlice = createSlice({
     decrement: (state) => {
       state.count -= 1;
     },
+    reset: (state) => {
+      state.count = 0;
+    },
+    incrementByAmount: (state, action) => {
+      state.count += action.payload;
+    },
   },
 });
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement, reset, incrementByAmount } =
+  counterSlice.actions;
+// exoirt the reducer
 export default counterSlice.reducer;
--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,10 @@
 // it is recommmended to have a single store rather than multiple stores
 // slice is multiple reducer functios and action fpr a single feature in our app
 import { configureStore } from "@reduxjs/toolkit";
-
-export const store = configureStore({ reducer: {} });
+import { counterSlice } from "./feature/counter/counterSlice";
+import counterReducer from "./feature/counter/counterSlice";
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
